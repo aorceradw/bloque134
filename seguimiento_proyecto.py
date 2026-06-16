7cbf4231575c68235139cf8b73e92d12bd14d877
Angela Orcera <aorceradw@gmail.com>
2026-06-16 12:42:31 +0200

    ejercicio a medias

---
+++ seguimiento_proyecto.py
@@ -0,0 +1,26 @@
+
+suma = 0
+tareas_terminadas = 0
+tareas_terminadas = 0
+tareas_no_terminadas = 0
+horas_estimadas = 0
+avance_proyecto = 0
+
+def almacenar_tareas():
+    """Programa para almacenar y calcular tareas"""
+    tareas = []
+    try :
+        for tarea in tareas:
+            contenido = tareas.append(tarea)
+            nombre = tareas.append("CODIFICAR")
+            responsable = tareas.append("Angela Orcera")
+            horas_estimadas = tareas.append("8")
+            return contenido
+
+        print(tareas)
+    except ValueError:
+        print("Valor incorrecto")
+    
+
+    
+    
